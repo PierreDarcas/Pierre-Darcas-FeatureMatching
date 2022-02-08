b8a10e4784535396f2de5db4fa2ed360496c6529
Pierre <pierre.darcas@hotmail.fr>
2022-02-08 08:40:56 +0100

    add images

---
+++ src/Pierre.Darcas.FeatureMatching/ObjectDetection.cs
@@ -0,0 +1,10 @@
+﻿namespace Pierre.Darcas.FeatureMatching;
+
+public class ObjectDetection
+{
+    public IList<ObjectDetectionResult> DetectObjectInScenes(byte[] objectImageData, IList<byte[]> imagesSceneData)
+    {
+        //TODO
+        throw new NotImplementedException();
+    }
+}
