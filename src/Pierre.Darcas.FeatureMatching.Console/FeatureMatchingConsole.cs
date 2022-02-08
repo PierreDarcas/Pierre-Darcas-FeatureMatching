e0d87dbccbbc1f9505de5558c6400e18ed88cada
Pierre <pierre.darcas@hotmail.fr>
2022-02-08 09:55:03 +0100

    console OK

---
+++ src/Pierre.Darcas.FeatureMatching.Console/FeatureMatchingConsole.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json;
+
+namespace Pierre.Darcas.FeatureMatching.Console;
+
+public class FeatureMatchingConsole
+{
+    static async Task Main(string[] args)
+    {
+        byte[] image = await File.ReadAllBytesAsync(args[0]);
+        
+        var imageScenesData = new List<byte[]>();
+        foreach (var imagePath in Directory.EnumerateFiles(args[1]))
+        {
+            var imageBytes = await File.ReadAllBytesAsync(imagePath);
+            imageScenesData.Add(imageBytes);
+        }
+
+        var featureMatchingLib = new ObjectDetection();
+
+        var featureMatchingResults = await featureMatchingLib.DetectObjectInScenes(image,imageScenesData);
+        
+        foreach (var objectDetectionResult in featureMatchingResults)
+        {
+            System.Console.WriteLine($"Points:{JsonSerializer.Serialize(objectDetectionResult.Points)}");
+        }
+
+    }
+}
